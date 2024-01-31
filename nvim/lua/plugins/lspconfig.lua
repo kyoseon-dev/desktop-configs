0083adb6e17096bec72dc25b61f07f85545a657a
kyoseon-dev <kyoseon.dev@gmail.com>
2024-02-01 02:42:31 +0900

    lsp for clang added.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -31,6 +31,9 @@ return {
 			},
 		})
 
+		lspconfig.clangd.setup({
+		})
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
