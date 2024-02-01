738f8c0b1d7a36f8123561bc75b5023195cbf4cf
kyoseon-dev <kyoseon.dev@gmail.com>
2024-02-02 00:43:22 +0900

    zsh plugins makes me happy.

--- zsh/user.zsh
+++ zsh/user.zsh
@@ -3,7 +3,16 @@ HISTFILE="$ZDOTDIR/histfile"
 HISTSIZE=1000
 SAVEHIST=1000
 
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history
+
 setopt autocd beep extendedglob nomatch notify
+
 bindkey -v
 # End of lines configured by zsh-newuser-install
 
@@ -40,3 +49,4 @@ alias ls="eza --icons=always"
 alias getp="ps waxf|grep -v grep|grep "
 alias nvim-config="(cd ~/.config/nvim; nvim -c ':Neotree filesystem reveal left' lua/vim-keymaps.lua)"
 alias nvi="nvim -c ':Neotree filesystem reveal left'"
+alias asp="cat ~/tmp/fedora.txt | fzf"
