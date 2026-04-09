034514b485be316542dbac10f324f58630308c31
kyoseon-dev <kyoseon.dev@gmail.com>
2026-04-09 20:14:25 +0900

    telescope refine.

--- nvim-pack/lua/plugins/telescope.lua
+++ nvim-pack/lua/plugins/telescope.lua
@@ -21,11 +21,22 @@ require("telescope").setup({
 		}
 	}
 })
+
 require("telescope").load_extension("ui-select")
 
 local builtin = require("telescope.builtin")
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fc', builtin.commands, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fo', builtin.vim_options, {})
+vim.keymap.set('n', '<leader>ft', builtin.colorscheme, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>fn', function()
+	builtin.find_files {
+		cwd = vim.fn.stdpath("config")
+	}
+end)
