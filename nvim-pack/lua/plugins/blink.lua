2a1951af81a224227037f0dbde8535f6c63e8037
kyoseon-dev <kyoseon.dev@gmail.com>
2026-04-14 18:15:52 +0900

    not lazyloading blink

--- nvim-pack/lua/plugins/blink.lua
+++ nvim-pack/lua/plugins/blink.lua
@@ -6,49 +6,37 @@ vim.pack.add({
 	"https://github.com/rafamadriz/friendly-snippets",
 })
 
-local group = vim.api.nvim_create_augroup("BlinkCmpLazyLoad", { clear = true })
-
-vim.api.nvim_create_autocmd('InsertEnter', {
-	pattern = "*",
-	group = group,
-	once = true,
-	callback = function()
-		require('blink.cmp').setup({
-			keymap = { preset = "super-tab" },
-			appearance = {
-				use_nvim_cmp_as_default = false,
-				nerd_font_variant = 'normal',
+require('blink.cmp').setup({
+	keymap = { preset = "super-tab" },
+	appearance = {
+		use_nvim_cmp_as_default = false,
+		nerd_font_variant = 'normal',
+	},
+	completion = {
+		keyword = { range = 'full' },
+		documentation = { auto_show = true, auto_show_delay_ms = 500, },
+		ghost_text = { enabled = true, },
+	},
+	signature = {
+		enabled = true,
+		window = {
+			show_documentation = true,
+			border = 'rounded',
+		},
+	},
+	sources = {
+		default = { 'lsp', 'path', 'snippets', 'buffer', },
+		per_filetype = {
+			org = { 'orgmode' }
+		},
+		providers = {
+			orgmode = {
+				name = 'Orgmode',
+				module = 'orgmode.org.autocompletion.blink',
+				fallbacks = { 'buffer' },
 			},
-			completion = {
-				keyword = { range = 'full' },
-				menu = {
-				--[[
-					draw = {
-						columns = {
-							{ 'label', 'label_description', gap = 1 },
-							{ 'kind_icon', 'kind' },
-						},
-					},
-				--]]
-				},
-				documentation = { auto_show = true, auto_show_delay_ms = 500, },
-				ghost_text = { enabled = true, },
-			},
-
-			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer', },
-				per_filetype = {
-					org = {'orgmode'}
-				},
-				providers = {
-					orgmode = {
-						name = 'Orgmode',
-						module = 'orgmode.org.autocompletion.blink',
-						fallbacks = { 'buffer' },
-					},
-				},
-			},
-			fuzzy = { implementation = "prefer_rust_with_warning" },
-		})
-	end,
+		},
+	},
+	cmdline = { enabled = true, },
+	fuzzy = { implementation = "prefer_rust_with_warning" },
 })
--- nvim-pack/lua/plugins/noice.lua
+++ nvim-pack/lua/plugins/noice.lua
@@ -13,6 +13,7 @@ require("noice").setup({
 			--["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 		},
 	},
+	cmdline = { enabled = false, },
 	presets = {
 		bottom_search = true, -- use a classic bottom cmdline for search
 		command_palette = true, -- position the cmdline and popupmenu together
