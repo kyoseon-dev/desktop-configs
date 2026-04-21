f0c7510ed3fb598e801801f4ac1d094a1fad973f
kyoseon-dev <kyoseon.dev@gmail.com>
2026-04-21 21:10:19 +0900

    org header custom faces.

--- emacs/init.el
+++ emacs/init.el
@@ -37,7 +37,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit outline-1 :height 1.6))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.38))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.23))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.1)))))
+ )
