c01139435fdc5fb773b705a9a44338ef65bdea76
kyoseon-dev <kyoseon.dev@gmail.com>
2026-05-03 17:03:26 +0900

    delete unused package (doom-modeline)

--- emacs/init.el
+++ emacs/init.el
@@ -5,6 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(font-use-system-font t)
  '(inhibit-startup-screen t)
+ '(org-capture-templates
+	'(("p" "Private templates")
+	  ("pt" "TODO Entry" entry
+		(file+headline "~/Documents/orgfiles/capture.org" "CAPTURE")
+		(file "~/.config/emacs/tpl/todo.txt") :empty-lines-before 1)))
  '(package-selected-packages nil))
 
 (setq comp-deferred-compilation t)
@@ -34,4 +39,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit outline-1 :foreground "#cdcdcd" :weight bold :height 1.8))))
+ '(org-level-1 ((t (:inherit outline-1 :foreground "#8b8bde" :height 1.38))))
+ '(org-level-2 ((t (:inherit outline-2 :foreground "#abbeef" :height 1.23))))
+ '(org-level-3 ((t (:inherit outline-3 :foreground "#adcbed" :height 1.1)))))
